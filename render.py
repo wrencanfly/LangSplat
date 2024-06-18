de40e928016f0bc47a74374ffd5d9f0aff866d51
Yingwei Song <56505931+wrencanfly@users.noreply.github.com>
2024-06-17 21:47:42 -0400

    Update render.py

--- render.py
+++ render.py
@@ -8,6 +8,7 @@
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
+import time
 import numpy as np
 import torch
 from scene import Scene
@@ -22,38 +23,62 @@ from arguments import ModelParams, PipelineParams, get_combined_args
 from gaussian_renderer import GaussianModel
 
 def render_set(model_path, source_path, name, iteration, views, gaussians, pipeline, background, args):
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
-    render_npy_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders_npy")
-    gts_npy_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt_npy")
 
-    makedirs(render_npy_path, exist_ok=True)
-    makedirs(gts_npy_path, exist_ok=True)
-    makedirs(render_path, exist_ok=True)
-    makedirs(gts_path, exist_ok=True)
 
-    frame_lst = [2, 25, 43, 107, 129, 140]
-    positives = ['stuffed bear', 'coffee mug', 'bag of cookies', 'sheep', 'apple', 'paper napkin', 'plate', 'tea in a glass', 'bear nose', 'three cookies', 'coffee']
-    for pos_index in range(1):
-        for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-            if idx + 1 in frame_lst:
-                output = render(view, gaussians, pipeline, background, pos_index, args)
-                if not args.include_feature:
-                    rendering = output["render"]
-                else:
-                    rendering = output["language_feature_image"]
-                    # print("rendering shape", rendering.shape) torch.Size([3, 730, 988])
+    frame_lst = [40, 104, 151, 194]
+
+    positives_lst = [['old camera', 'toy elephant', 'waldo', 'tesla door handle', 'porcelain hand', 'rubber duck with hat', 'rubber duck with buoy', 'pink ice cream', 'red toy chair', 'green apple', 'pikachu', 'red apple', 'spatula', 'jake', 'toy cat statue', 'pirate hat', 'miffy'],
+                    ['rubics cube', 'green apple', 'green toy chair', 'jake', 'old camera', 'pink ice cream', 'pumpkin', 'red apple', 'rubber duck with hat', 'tesla door handle', 'spatula', 'rubber duck with buoy', 'pirate hat'],
+                    ['rubber duck with hat', 'rubics cube', 'toy elephant', 'green apple', 'jake', 'toy cat statue', 'pikachu', 'porcelain hand', 'red apple', 'waldo', 'pirate hat'],
+                    ['toy elephant', 'pink ice cream', 'porcelain hand', 'green apple', 'green toy chair', 'old camera', 'rubics cube', 'spatula', 'toy cat statue', 'waldo', 'rubber duck with buoy', 'pirate hat', 'miffy', 'bag', 'rubber duck with hat']]
+    
+    # positives = ['stuffed bear', 'coffee mug', 'bag of cookies', 'sheep', 'apple', 'paper napkin', 'plate', 'tea in a glass', 'bear nose', 'three cookies', 'coffee']
+    for scene_index, frame in enumerate(frame_lst):
+        scene_folder = os.path.join(model_path, name, 'scene_{}'.format(frame))
+        os.makedirs(scene_folder, exist_ok=True)
+        
+        positives = positives_lst[scene_index]
+        for pos_index, positive in enumerate(positives):
+            render_path = os.path.join(scene_folder, '{}'.format(positive), "ours_{}".format(iteration), "renders")
+            gts_path = os.path.join(scene_folder, '{}'.format(positive), "ours_{}".format(iteration), "gt")
+            render_npy_path = os.path.join(scene_folder, '{}'.format(positive), "ours_{}".format(iteration), "renders_npy")
+            gts_npy_path = os.path.join(scene_folder, '{}'.format(positive), "ours_{}".format(iteration), "gt_npy")
+
+            os.makedirs(render_npy_path, exist_ok=True)
+            os.makedirs(gts_npy_path, exist_ok=True)
+            os.makedirs(render_path, exist_ok=True)
+            os.makedirs(gts_path, exist_ok=True)
+
+            for idx, view in enumerate(tqdm(views, desc="Rendering progress for scene {}".format(frame))):
+                if idx == frame:
+
+                    start_time = time.time()
+                    output = render(scene_index, view, gaussians, pipeline, background, pos_index, args)
+                    end_time = time.time()
+                    loading_time = end_time - start_time
+                    print(f"Time taken to render: {loading_time:.4f} seconds")
                     
-                if not args.include_feature:
-                    gt = view.original_image[0:3, :, :]
-                    
-                else:
-                    gt, mask = view.get_language_feature(os.path.join(source_path, args.language_features_name), feature_level=args.feature_level)
+                    if not args.include_feature:
+                        rendering = output["render"]
+                    else:
+                        rendering = output["language_feature_image"]
 
-                np.save(os.path.join(render_npy_path, '{0:05d}'.format(idx) + ".npy"),rendering.permute(1,2,0).cpu().numpy())
-                np.save(os.path.join(gts_npy_path, '{0:05d}'.format(idx) + ".npy"),gt.permute(1,2,0).cpu().numpy())
-                torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-                torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+                    if not args.include_feature:
+                        gt = view.original_image[0:3, :, :]
+                    else:
+                        gt, mask = view.get_language_feature(os.path.join(source_path, args.language_features_name), feature_level=args.feature_level)
+
+                    assert False
+                    print("rendering shape", rendering.shape)
+                    np.save(os.path.join(render_npy_path, '{0:05d}'.format(idx) + ".npy"), rendering.permute(1, 2, 0).cpu().numpy())
+                    # np.save(os.path.join(gts_npy_path, '{0:05d}'.format(idx) + ".npy"), gt.permute(1, 2, 0).cpu().numpy())
+                    
+                    # print("rendering shape", rendering.shape) # rendering shape torch.Size([5, 730, 988]) [03/06 01:12:21]
+                    
+                    img = rendering[0, :, :].unsqueeze(0).repeat(3, 1, 1)
+                    torchvision.utils.save_image(img, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+                    # torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+
                 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, args):
     with torch.no_grad():
